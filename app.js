36dfb7fc7f9344ddd1cfd185a5e3e5247313e149
Saheed Shittu <shittusaheed01@gmail.com>
2023-01-01 16:16:33 +0100

    removed comments

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ require('dotenv').config()
 
 //Rate Limiting
 const limiter = rateLimit({
-	windowMs: 0.5 * 60 * 1000, // 15 minutes
-	max: 4, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+	windowMs: 0.5 * 60 * 1000,
+	max: 4,
+	standardHeaders: true,
+	legacyHeaders: false,
 })
 
 //security
